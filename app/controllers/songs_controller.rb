10157fb648e996df4252281385a4550e4dc87fc0
Emily Morgan <emorgan05@gmail.com>
2017-11-09 21:11:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -31,7 +31,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params["song"]["artist"]["name"])
     @song.genre_ids = params["song"]["genre_ids"]
     @song.save
-    
+
     flash[:message] = "Successfully edited song."
     redirect "/songs/#{@song.slug}"
   end
